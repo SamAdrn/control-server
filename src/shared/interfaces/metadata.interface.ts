872141e4faffaf7363e069b2f4ddb4e23b92c045
Samuel Kosasih <samadrn@umd.edu>
2024-08-17 00:27:43 -0400

    Generalized logic using metadata

---
+++ src/shared/interfaces/metadata.interface.ts
@@ -0,0 +1,9 @@
+export interface Metadata {
+    name: string;
+    namePlural: string;
+    label: string;
+    labelPlural: string;
+    description: string;
+    keyName: string;
+    sortBy: string[];
+}
